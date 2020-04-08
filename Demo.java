e566a61b44da793d979d7030bbcac227804e7414
FelixvL <digitaleservice@gmail.com>
2020-04-08 11:17:20 +0200

    eerste commit

---
+++ Demo.java
@@ -0,0 +1,14 @@
+class Demo{
+	public static void main(String[] args){
+		System.out.println("Hoi2");
+	}
+}
+class Auto{
+	public static void main(String[] args){
+		System.out.println("Hoi3");
+	}
+}
+
+
+
+//0615517962
